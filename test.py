88dc7eaec5b429121774e21f0d93bb7c1786a751
Bendron <b.caudron@student.uclouvain.be>
2021-03-20 15:02:06 +0100

    enemy fait des rondes

--- test.py
+++ test.py
@@ -18,14 +18,16 @@ playerImg = pygame.image.load('ressources/images/player.png')
 sizePlayer = 64
 playerX = 370
 playerY = 480
+playerSpeed = 0.5
 playerX_change = 0
 playerY_change = 0
 
 # Enemy
 enemyImg = pygame.image.load('ressources/images/joker.png')
+sizeEnemy = 64
 enemyX = 370
 enemyY = 50
-enemyX_change = 0
+enemyX_change = 0.3
 enemyY_change = 0
 
 def player(x,y):
@@ -47,16 +49,16 @@ while running :
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.1
+                playerX_change = -playerSpeed
                 # playerX-=20
             elif event.key == pygame.K_RIGHT:
-                playerX_change = 0.1
+                playerX_change = playerSpeed
                 # playerX+=20
             elif event.key == pygame.K_DOWN:
-                playerY_change = 0.1
+                playerY_change = playerSpeed
                 # playerY+=20
             elif event.key == pygame.K_UP:
-                playerY_change = -0.1
+                playerY_change = -playerSpeed
                 # playerY-=20
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -67,7 +69,10 @@ while running :
     playerX+=playerX_change
     playerY+=playerY_change
 
-    # Boundaries
+    enemyX+=enemyX_change
+    enemyY+=enemyY_change
+
+    # Boundaries player
     if playerX <= 0 :
         playerX = 0
     elif playerX >= sizeX-sizePlayer:
@@ -77,6 +82,19 @@ while running :
         playerY = 0
     elif playerY >= sizeY-sizePlayer:
         playerY = sizeY-sizePlayer
+
+    # Boundaries enemy
+    if enemyX <= 0 :
+        enemyX = 0 
+        enemyX_change = -enemyX_change       
+    elif enemyX >= sizeX-sizePlayer:
+        enemyX_change = -enemyX_change
+        # enemyX = sizeX-sizePlayer
+
+    if enemyY <= 0 :
+        enemyX_change = -enemyX_change
+    elif enemyY >= sizeY-sizeEnemy:
+        enemyY = sizeY-sizeEnemy
 
     player(playerX,playerY)
     enemy(enemyX,enemyY)
